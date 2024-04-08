431c7eca776cbbcccc4aeafe50c4be23036d998a
Serenay <serenayygunes@gmail.com>
2024-04-08 21:05:07 +0300

    Loading meals data into a screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meals_app/data/dummy_data.dart';
+import 'package:flutter_meals_app/model/meal.dart';
 import 'package:flutter_meals_app/screens/categories.dart';
+import 'package:flutter_meals_app/screens/meals.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final theme = ThemeData(
@@ -22,7 +25,10 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      home: const MealsScreen(
+        title: 'Some category',
+        meals: dummyMeals,
+      ),
     );
   }
 }
--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -8,10 +8,38 @@ class MealsScreen extends StatelessWidget {
   final List<Meal> meals;
   @override
   Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: ((context, index) => Text(meals[index].title)));
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'nothing here',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Try selecting a different category',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            )
+          ],
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
-        title: Text(),
+        title: Text(title),
       ),
+      body: content,
     );
   }
 }
